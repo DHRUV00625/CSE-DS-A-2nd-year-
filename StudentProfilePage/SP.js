cd2562d7c9feec5eef0f5f8bae6cf2e3024bc883
DHRUV00625 <dhruv.25b15410073@abes.ac.in>
2026-07-22 10:21:58 +0530

    lets see

---
+++ StudentProfilePage/SP.js
@@ -0,0 +1,10 @@
+const form= document.getElementById("studentForm");
+form.addEventListener(Submit){
+     const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const message = document.getElementById("message").value;
+
+    
+
+}
